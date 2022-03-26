9f96415538733b7436c469d43b871897d4498818
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-03-26 20:30:24 +0100

    Switch back to schema files to support rover supergraph compose
    This reverts commit 7fc5142f664205ca0ffd923f332db5b1d1c39354.

--- src/products/server.ts
+++ src/products/server.ts
@@ -1,14 +1,15 @@
 import { buildSubgraphSchema } from "@apollo/subgraph"
-import { ApolloServer } from "apollo-server"
+import { ApolloServer, gql } from "apollo-server"
 import { ApolloServerPluginInlineTrace } from "apollo-server-core"
+import { readFileSync } from "fs"
 import { resolvers } from "./resolvers"
-import { typeDefs } from "./schema"
 
 const name = "Products subgraph"
 const port = 3002
 const introspection = true
 const plugins = [ApolloServerPluginInlineTrace()]
 
+const typeDefs = gql(readFileSync(__dirname + "/schema.gql", "utf8"))
 const schema = buildSubgraphSchema({ typeDefs, resolvers })
 const server = new ApolloServer({ schema, introspection, plugins })
 
